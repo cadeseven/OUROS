7af90eb346f0d63950771041e746fe0d1792402e
cadeseven <42783210+cadeseven@users.noreply.github.com>
2019-02-11 01:59:22 -0800

    Add files via upload

---
+++ sketch3.js
@@ -0,0 +1,30 @@
+
+let angle = 0;
+
+
+function setup() {
+  createCanvas(windowWidth - windowWidth*.03, windowHeight - windowHeight*.1, WEBGL);
+  fullscreen()
+
+  if (windowWidth > 720) {
+    snake = loadModel('snakesmooth.obj')
+  }
+
+  else {
+    snake = loadModel('snakesmall.obj');
+  }
+
+
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth - windowWidth*.03, windowHeight - windowHeight*.1);
+}
+
+function draw() {
+  background(256, 256, 256);
+  normalMaterial(100, 200, 150);
+  orbitControl();
+  model(snake);
+  angle += .01;
+}
